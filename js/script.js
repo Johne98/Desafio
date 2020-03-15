03dee3f0f4c356c27260767cbcd189d121bd0354
Johne98 <59737549+Johne98@users.noreply.github.com>
2020-03-15 16:08:28 -0300

    Formulário EloGroup

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document
+    .querySelector('body button')
+    .addEventListener("click", function () {
+        document
+            .querySelector('.form')
+            .classList.toggle('hide')
+    });
